2b75e8fa3fae23ad58fa4c7e509819fa92a78d08
pswierzbinski <pawel.swierzbinski22@gmail.com>
2023-12-07 12:16:14 +0100

    Beautify

--- src/app/filter-priority.pipe.ts
+++ src/app/filter-priority.pipe.ts
@@ -6,12 +6,12 @@ import { Task } from './task';
 })
 export class FilterPriorityPipe implements PipeTransform {
 
-  transform(list: Task[], filterPrio: number, isSet:boolean, filterDone:boolean): any {
-    
-    if(isSet && filterDone)
-    list=list.filter((elem:Task)=>elem.priority==filterPrio);
-    if(filterDone)
-    list=list.filter((elem:Task)=>elem.isDone==false)
+  transform(list: Task[], filterPrio: number, isSet: boolean, filterDone: boolean): Task[] {
+
+    if (isSet && filterDone)
+      list = list.filter((elem: Task) => elem.priority == filterPrio);
+    if (filterDone)
+      list = list.filter((elem: Task) => elem.isDone == false)
     return list;
   }
 
