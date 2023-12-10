0597a989d50ba5d7e31ccc7304a09317c3e74e56
pswierzbinski <pawel.swierzbinski22@gmail.com>
2023-12-10 17:50:18 +0100

    Changed dropdown menu to show boards, routing fix

--- src/app/board-list/board-list.component.ts
+++ src/app/board-list/board-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, Output, inject } from '@angular/core';
 import { Board } from '../board';
 import { BoardListService } from '../board-list.service';
 import { RouterModule } from '@angular/router';
@@ -10,10 +10,10 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./board-list.component.css'],
 })
 export class BoardListComponent {
-  boards: Board[] = [];
-  boardsService: BoardListService = inject(BoardListService);
-  
-  constructor(){
-    this.boards = this.boardsService.getBoards();
-  }
+
+  // boards: Board[] = [];
+  // boardsService: BoardListService = inject(BoardListService);
+  // constructor(){
+  //   this.boards = this.boardsService.getBoards();
+  // }
 }
