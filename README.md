02555b8edab34fe3e379af5cba7ea8d0fdf1e3cc
piotus <piotrradziszewski@outlook.com>
2023-12-14 20:34:07 +0100

    db.json works (calls db.json way too many times, but w/e) + small fixes

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # MyWorkBoard
 
-WIP XD
+installing packages: npm install
+installing json-server: npm install -g json-server 
+launching db.json: json-server --watch db.json
+launching app: ng serve --open
+
+Powered by MyPHPoL Inc.
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,14 +4,13 @@ import { Board } from '../board';
 import { NewBoardComponent } from '../new-board/new-board.component';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 
-
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class HeaderComponent implements OnInit{
+export class HeaderComponent implements OnInit {
   boards: Board[] = [];
   boardsService: BoardListService = inject(BoardListService);
   @Output() newItemEvent = new EventEmitter<number>();
@@ -19,72 +18,68 @@ export class HeaderComponent implements OnInit{
   menuStatus: boolean = false;
   currStyle: string = 'blue';
 
-  constructor(private renderer: Renderer2, private elRef: ElementRef, public dialog: MatDialog){
+  constructor(private renderer: Renderer2, private elRef: ElementRef, public dialog: MatDialog) {
     this.renderer.addClass(document.body, 'blue');
-    this.boards = this.boardsService.getBoards();
-
-    /* THIS \/\/ could be used with working HTTP requests, but since we don't have a working backend this won't be utilized at the moment
     this.boardsService.getBoards().subscribe(boards => this.boards = boards);
-    it needs to be pointed that this function works, but since we don't have POST nor PATCH boards won't update
-    (which is crucial for the presentation) */
-    
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
   SideNavToggle() {
     this.menuStatus = !this.menuStatus;
     this.sideNavToggled.emit(this.menuStatus);
   }
 
-
-
   // popup modal dialog
-  openDialog(opt: string, id:string) : void {
+  openDialog(opt: string, id: string): void {
     let dialogRef = null;
     const dialogConfig = new MatDialogConfig();
 
     dialogConfig.disableClose = true;
     dialogConfig.autoFocus = true;
-    dialogConfig.width='260px';
-    dialogConfig.height='260px';
-    dialogConfig.data= {name: ''}; // data we give to the dialog window
+    dialogConfig.width = '260px';
+    dialogConfig.height = '260px';
+    dialogConfig.data = { name: '' }; // data we give to the dialog window
 
-    
-    if(opt === 'editBoard') // edit card
+
+    if (opt === 'editBoard') // edit board
     {
-      dialogConfig.data={name: this.boardsService.getBoard(id).name}; // data we give to the dialog window
+      let eBoard: Board = {} as Board;
+      this.boardsService.getBoard(id).subscribe(board => {
+        eBoard = board;
 
-      dialogRef = this.dialog.open(NewBoardComponent,dialogConfig); // opens dialog window
+        dialogConfig.data = { name: eBoard.name }; // data we give to the dialog window
 
-      // happens after user clicks 'Accept'
+        dialogRef = this.dialog.open(NewBoardComponent, dialogConfig); // opens dialog window
+
+        // happens after user clicks 'Accept'
+        dialogRef.afterClosed().subscribe(result => {
+          if (result !== undefined) {
+            console.log(result.name);
+            this.boardsService.editBoard(id, result.name).subscribe(ret => this.boards[this.boards.findIndex(b => b.Id == id)].name = result.name);
+          }
+        });
+      });
+    } else if (opt === 'addBoard') {
+      dialogRef = this.dialog.open(NewBoardComponent, dialogConfig); // opens dialog window
+
       dialogRef.afterClosed().subscribe(result => {
         if (result !== undefined) {
-          console.log(result.name, result.priority, result.color);
-          this.boardsService.getBoard(id).name = result.name;
+          console.log(result.name,);
+          var newBoard = new Board(undefined, undefined, result.name,);
+          this.boardsService.addBoard(newBoard).subscribe(ret => this.boards.push(newBoard));
         }
       });
-    } else if(opt === 'addBoard'){
-      dialogRef = this.dialog.open(NewBoardComponent,dialogConfig); // opens dialog window
-  
-      dialogRef.afterClosed().subscribe(result => {
-      if (result !== undefined) {
-        console.log(result.name,);
-        var newBoard = new Board(undefined, undefined, result.name,);
-        this.boardsService.addBoard(newBoard);
-      }
-    });
     }
-    
   }
+
   deleteBoard(value: string): void {
-    if(confirm("Are you sure you want to delete this board?")) {
-      this.boardsService.deleteBoard(value);
+    if (confirm("Are you sure you want to delete this board?")) {
+      this.boardsService.deleteBoard(value).subscribe(ret => this.boards.splice(this.boards.findIndex(b => b.Id == value), 1));
     }
   }
 
-  ChangeTheme(opt: string){
-
+  ChangeTheme(opt: string) {
     this.renderer.removeClass(document.body, this.currStyle);
     this.currStyle = opt;
     this.renderer.addClass(document.body, this.currStyle);
