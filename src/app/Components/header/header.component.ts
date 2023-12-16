0f146a51ee0235b1d4f4690790651245be5c2b71
pswierzbinski <pawel.swierzbinski22@gmail.com>
2023-12-16 23:08:17 +0100

    (not) working routing when deleting currently open board

--- src/app/Components/header/header.component.ts
+++ src/app/Components/header/header.component.ts
@@ -1,8 +1,9 @@
-import { Component, ElementRef, EventEmitter, OnInit, Output, Renderer2, ViewChild, ViewEncapsulation, inject } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, Renderer2, ViewChild, ViewEncapsulation, inject, Input } from '@angular/core';
 import { BoardListService } from '../../Services/board-list.service';
 import { Board } from '../../board';
 import { NewBoardComponent } from '../new-board/new-board.component';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -17,13 +18,16 @@ export class HeaderComponent implements OnInit {
   @Output() sideNavToggled = new EventEmitter<boolean>();
   menuStatus: boolean = false;
   currStyle: string = 'blue';
+  boardID:string = ''; //used in order to redirect if currently open board is deleted
 
-  constructor(private renderer: Renderer2, private elRef: ElementRef, public dialog: MatDialog) {
+  constructor(private renderer: Renderer2, private elRef: ElementRef, public dialog: MatDialog, private router: Router) {
     this.renderer.addClass(document.body, 'blue');
     this.boardsService.getBoards().subscribe(boards => this.boards = boards);
   }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.boardID = this.router.url.slice(7);
+   }
 
   SideNavToggle() {
     this.menuStatus = !this.menuStatus;
@@ -73,15 +77,29 @@ export class HeaderComponent implements OnInit {
     }
   }
 
+  test(value: string): boolean{
+    if (this.router.url.slice(7) == value ) {
+      this.deleteBoard(value);
+      return true;
+    }else{
+      this.deleteBoard(value);
+    return false;
+    }
+
+  }
   deleteBoard(value: string): void {
     if (confirm("Are you sure you want to delete this board?")) {
       this.boardsService.deleteBoard(value).subscribe(ret => this.boards.splice(this.boards.findIndex(b => b.Id == value), 1));
     }
+    console.log(this.boardID);
+      console.log(value);
   }
 
   ChangeTheme(opt: string): void {
     this.renderer.removeClass(document.body, this.currStyle);
     this.currStyle = opt;
     this.renderer.addClass(document.body, this.currStyle);
+    console.log(this.boardID)
+    console.log(this.router.url.slice(7));
   }
 }
